44e79e1cd6e8b6ea592f716d143ab34a97d87353
SupremeNoble <b022110163@student.utem.edu.my>
2023-12-25 22:14:37 +0800

    Port change fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
-//const port = process.env.PORT || 3000;
-const port = 3000;
+const port = process.env.PORT || 3000;
+//const port = 3000;
 const jwt = require('jsonwebtoken');
 
 // MongoDB connection URL
